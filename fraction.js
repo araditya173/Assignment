a3a652e0f611d651d8bf352d6918a96497187f04
Manjunath2107 <117253262+Manjunath2107@users.noreply.github.com>
2023-04-18 10:56:02 +0530

    Update fraction.js

--- fraction.js
+++ fraction.js
@@ -6,13 +6,20 @@ class Fraction
       this.numerator=numerator;
       this.denominator=denominator;
     }
-    sub(f)
+    add(f)
     {
       let den = this.denominator * f.denominator;
-      let num = this.numerator * f.denominator - f.numerator * this.denominator;
+      let num = this.numerator * f.denominator + f.numerator * this.denominator;
       let g = gcd(num,den);
       console.log(g);
       return new Fraction(num/g,den/g);
+    }
+    multiply(f)
+    {
+      let num = this.numerator*f.numerator;
+      let den = this.denominator*f.denominator;
+      console.log(num,den);
+      return new Fraction(num,den);
     }
   }
 function gcd(a, b)
@@ -28,6 +35,8 @@ function gcd(a, b)
       }
     return a;
 }
+
+   
 function input_fraction()
 {
     let numerator = parseInt(prompt("enter the numerator of the fraction: "));
@@ -38,21 +47,22 @@ function input_fraction()
 // {
 //   let a1,a2,b1,b2;
 //   [a1,b1,a2,b2]=input();
-//   let fraction=sub(a1,b1,a2,b2);
+//   let fraction=add(a1,b1,a2,b2);
+//   let 
 //   console.log("the addition of two fraction is %d/%d",fraction.numerator,fraction.denominator);
 
 function output_result(f1,f2,result)
 {
   console.log("%d/%d + %d/%d is %d/%d", f1.numerator,f1.denominator, f2.numerator, f2.denominator, result.numerator, result.denominator);
-  
 }
 
 function main()
 {
   let f1 = input_fraction();
   let f2 = input_fraction();
-  let f3 = f1.sub(f2)
-  output_result(f1,f2,f3);
+  let f3 = f1.add(f2)
+  let f4 = f1.multiply(f1,f2);
+  output_result(f1,f2,f3,f4);
 }
 
 main();
